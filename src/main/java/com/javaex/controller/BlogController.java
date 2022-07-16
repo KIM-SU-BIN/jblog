13710a45424181d0657309cfb06815c91c54b2b3
subintoto <subintoto@naver.com>
2022-07-16 16:22:46 +0900

    2022-07-16

--- src/main/java/com/javaex/controller/BlogController.java
+++ src/main/java/com/javaex/controller/BlogController.java
@@ -1,5 +1,37 @@
 package com.javaex.controller;
 
-public class BlogController {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.service.BlogService;
+
+@Controller
+public class BlogController {
+	
+	//필드
+	@Autowired
+	BlogService blogService;
+
+	//생성자
+	
+	//메소드
+	
+	//메소드 일반
+	
+	//메인화면 class는 대문자!
+	@RequestMapping(value = "/{id}", method = {RequestMethod.GET, RequestMethod.POST})
+	public String main(Model model, @PathVariable String id) {
+		System.out.println("BlogController>main");
+		
+		Map<String, Object> blogMap = BlogService.getBlog(id);
+		model.addAttribute("blogMap", blogMap);
+		
+		return "blog/blog-main";
+	}
 }
--- src/main/java/com/javaex/service/BlogService.java
+++ src/main/java/com/javaex/service/BlogService.java
@@ -1,5 +1,29 @@
 package com.javaex.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.dao.BlogDao;
+
+@Service
 public class BlogService {
+
+	//필드
+	@Autowired
+	BlogDao blogDao;
+
+	public static Map<String, Object> getBlog(String id) {
+		System.out.println("BlogService>getBlog");
+		
+		return null;
+	}
+	
+	//생성자
+	
+	//메소드
+	
+	//메소드 일반
 
 }
